bd34814bd25b2ee66a2a9d7c7fc507aad879365d
poppo <kzkdrmlove@gmail.com>
2023-08-18 10:53:43 +0900

    オブジェクトと分割代入

--- js/script.js
+++ js/script.js
@@ -1,12 +1,46 @@
 const menu = document.querySelector('#menu');
 
+// オブジェクトを指定する
+// const strawberry = {
+//     name: 'イチゴ',
+//     img: 'strawberry.jpg',
+//     price: 450,
+// };
+
+// console.log(strawberry.name);
+// console.log(strawberry['img']);
+
 const lists  = [
-    'strawberry.jpg',
-    'mango.jpg',
-    'lime.jpg',
-    'lemon.jpg',
-    'fig.jpg',
-    'apple.jpg',
+    {
+        name: 'イチゴ',
+        img: 'strawberry.jpg',
+        price: 450,
+    },
+    {
+        name: 'ライム',
+        img: 'lime.jpg',
+        price: '400',
+    },
+    {
+        name: 'マンゴー',
+        img: 'mango.jpg',
+        price: '500',
+    },
+    {
+        name:'レモン',
+        img: 'lemon.jpg',
+        price: '400',
+    },
+    {
+        name: 'イチジク',
+        img: 'fig.jpg',
+        price: '500',
+    },
+    {
+        name: 'リンゴ',
+        img: 'apple.jpg',
+        price:'400',
+    },
 ];
 // 
 
@@ -21,7 +55,13 @@ const lists  = [
 // console.log(lists.length);
 // 上のコードをforの繰り返しでスッキリさせる
 for(let i = 0; i < lists.length; i++){
-    const content = `<div><img src="images/${lists[i]}" alt=""></div>`;
+    // const name = lists[i].name;
+    // const img = lists[i].img;
+    // const price = lists[i].price;
+    // 分割代入で短くする
+    const {name, img, price} = lists[i];
+
+    const content = `<div><img src="images/${img}" alt=""><h2>${name}</h2><p>${price}円</p></div>`;
     menu.insertAdjacentHTML('beforeend', content);
 }
 // contentをmenuにHTML要素として表示させる。第一引数にHTML内の挿入位置、第二引数に挿入したい内容を書く。
